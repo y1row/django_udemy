cfe4311e09554ee8ba905ff5b86b0adde4039e11
y1row <yonesuke777@gmail.com>
2018-10-27 22:56:51 +0900

    投稿モデル

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField('タイトル', max_length=200)
+    text = models.TextField('本文')
+    created_at = models.DateTimeField('日付', default=timezone.now)
