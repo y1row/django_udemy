ad60f8a58877c4f87d02a2bc51aad55b55c34002
y1row <yonesuke777@gmail.com>
2018-10-28 01:32:37 +0900

    登録日時と更新日時を持ってみる

--- diary/models.py
+++ diary/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from django.utils import timezone
 
 
 class Diary(models.Model):
     id = models.BigAutoField(primary_key=True)
     title = models.CharField('タイトル', max_length=200)
     text = models.TextField('本文')
-    created_at = models.DateTimeField('日付', default=timezone.now)
+    created_at = models.DateTimeField('登録日時', auto_now_add=True)
+    updated_at = models.DateTimeField('更新日時', auto_now=True)
 
     class Meta:
         verbose_name = '日記'
