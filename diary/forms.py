240f8b143607b2ad437d95755d5eb7fee0024e70
y1row <yonesuke777@gmail.com>
2018-10-28 21:52:30 +0900

    投稿画面整いました

--- diary/forms.py
+++ diary/forms.py
@@ -7,3 +7,12 @@ class DiaryCreateForm(forms.ModelForm):
     class Meta:
         model = Diary
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # class
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+        # placeholder
+        self.fields['title'].widget.attrs['placeholder'] = 'タイトルを入力してください'
+        self.fields['text'].widget.attrs['placeholder'] = '本文を入力してください'
